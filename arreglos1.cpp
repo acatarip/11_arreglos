318034f0fdc4c548f9fd04058fbdcd3aeb30e675
acatarip <acatarip@unjbg.edu.pe>
2024-07-01 11:28:45 -0500

    Crear arreglos1.cpp

---
+++ arreglos1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main (){
+    system("cls");
+    int ss=0, n;
+    float pp=0, p100;
+
+    cout<<"Ingrese la cantidad de numeros para el arreglo: "; cin>>n;
+    int A[n];
+
+    if (n>0){
+        for (int i=0;i<n;i++){
+            cout<<"Ingrese valor "<<i+1<<": "; cin>>A[i];
+            ss=ss+A[i];
+            if (A[i]==0){
+                pp=pp+1;
+            }
+        }
+        p100=(pp/n)*100;
+        cout<<"La sumatoria de los numeros es "<<ss<<endl;
+        cout<<"El porcentaje de ceros en el arreglo es del "<<p100<<"%";
+
+    }
+    else {
+        cout<<"Cantidad de numeros para el arreglo no valido.";
+    }
+    return 0;
+}
