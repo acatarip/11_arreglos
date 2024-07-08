4c10c66800dc9da1ddd653a4f47d99f34a0d6573
acatarip <acatarip@unjbg.edu.pe>
2024-07-08 00:21:45 -0500

    arreglos2.cpp : Se agregaron comentarios

--- arreglos2.cpp
+++ arreglos2.cpp
@@ -1,19 +1,21 @@
+// Programa que pide n elementos, los separa en dos arreglos PAR e IMP
+// y escribe los elementos de cada arreglo por separado
 #include <iostream>
 using namespace std;
 int main (){
 	int pp=0, ii=0, n, i, j, k;
 	cout<<"Ingrese la cantidad de elementos: "; cin>>n; cout<<endl;
-	int A[n], PAR[n], IMP[n];
-	
+	int A[n], PAR[n], IMP[n];  //arreglos de n elementos
+	//Leer arreglo principal
 	for (i=0; i<n; i++){
 		cout<<"Ingrese el elemento "<<i+1<<": "; cin>>A[i];
 		if (A[i] % 2 == 0){
 			pp++;
-			PAR[pp] = A[i];
+			PAR[pp] = A[i];  //Leer arreglo de numeros pares
 		}
 		else{
 			ii++;
-			IMP[ii] = A[i];
+			IMP[ii] = A[i];  //Leer arreglo de numeros impares
 		}
 	}
 	system("PAUSE");
