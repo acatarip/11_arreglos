d08b662fbbfc185a304e835ddbbe2d9b7af4f203
acatarip <acatarip@unjbg.edu.pe>
2024-07-08 02:26:02 -0500

    Crear arreglos4.cpp

---
+++ arreglos4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void MULTI(int a[], int b);
+
+int main(){
+	int n;
+	cout<<"Indique longitud del arreglo: "; cin>>n;
+	int A[n];
+	MULTI(A, n); 
+	
+	for (int j=0; j<n; j++){
+		cout<<A[j]<<" ";
+	}
+	return 0;
+}
+
+void MULTI(int a[], int b){
+	int c;
+	cout<<"Ingrese un numero: "; cin>>c;
+	for (int i=0; i<b; i++){
+		a[i] = c * (i+1);
+	}
+	
+}
