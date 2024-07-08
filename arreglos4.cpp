e46c306759923ae857168ab085f1bec8d5e50135
acatarip <acatarip@unjbg.edu.pe>
2024-07-08 02:49:05 -0500

    arreglos4.cpp : Se movió la validación de c al programa principal, se mejoró los mensajes de información y se agregó espaciados al ejecutar el programa

--- arreglos4.cpp
+++ arreglos4.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 using namespace std;
 
-void MULTI(int a[], int b);
+void MULTI(int a[], int b, int c);
 
 int main(){
-	int n;
+	int n, mm;
 	cout<<"Indique longitud del arreglo: "; cin>>n;
 	while (n<=0){
 		cout<<"Longitud invalida."<<endl;
 		cout<<"Indique longitud del arreglo: "; cin>>n;
 	}
-	int A[n];
-	MULTI(A, n); 
 	
+	
+	cout<<"Ingrese un numero: "; cin>>mm;
+	while (mm<=0){
+		cout<<"Numero no valido."<<endl;
+		cout<<"Ingrese un numero: "; cin>>mm;
+	}
+	
+	cout<<endl;
+	int A[n];
+	MULTI(A, n, mm);
+	cout<<endl<<"Los primeros "<<n<<" multiplos de "<<mm<<" son: ";
 	for (int j=0; j<n; j++){
 		cout<<A[j]<<" ";
 	}
 	return 0;
 }
 
-void MULTI(int a[], int b){
-	int c;
-	cout<<"Ingrese un numero: "; cin>>c;
-	while (c<=0){
-		cout<<"Numero no valido."<<endl;
-		cout<<"Ingrese un numero: "; cin>>c;
-	}
+void MULTI(int a[], int b, int c){
 	for (int i=0; i<b; i++){
 		a[i] = c * (i+1);
 	}
