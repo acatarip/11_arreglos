9a10be8672fe6d46aa7ed3483dba91b812807d5f
acatarip <acatarip@unjbg.edu.pe>
2024-07-08 00:13:43 -0500

    arreglos2.cpp : Se agregaron mensajes de informacion al ejecutar el programa

--- arreglos2.cpp
+++ arreglos2.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main (){
 	int pp=0, ii=0, n, i, j, k;
-	cout<<"Ingrese cantidad de numeros: "; cin>>n;
+	cout<<"Ingrese la cantidad de elementos: "; cin>>n; cout<<endl;
 	int A[n], PAR[n], IMP[n];
 	
 	for (i=0; i<n; i++){
-		cout<<"Ingrese "<<i+1<<": "; cin>>A[i];
+		cout<<"Ingrese el elemento "<<i+1<<": "; cin>>A[i];
 		if (A[i] % 2 == 0){
 			pp++;
 			PAR[pp] = A[i];
@@ -19,13 +19,15 @@ int main (){
 	system("PAUSE");
 	cout<<endl;
 	
+	cout<<"Lista de los elementos impares:";
 	for (j=1; j<=ii; j++){
-		cout<<IMP[j]<<" ";
+		cout<<" "<<IMP[j];
 	}
 	cout<<endl;
 	
+	cout<<"Lista de los elementos pares:";
 	for (k=1; k<=pp; k++){
-		cout<<PAR[k]<<" ";
+		cout<<" "<<PAR[k];
 	}
 	return 0;
 }
